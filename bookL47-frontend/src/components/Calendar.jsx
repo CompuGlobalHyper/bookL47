3cb019e66e86e58b4cbd628c154440029e53eae6
CompuGlobalHyper <phineascrisp@gmail.com>
2026-06-03 16:03:08 -0700

    Added function to select a date

--- bookL47-frontend/src/components/Calendar.jsx
+++ bookL47-frontend/src/components/Calendar.jsx
@@ -1,16 +1,31 @@
 import React, { useState } from 'react'
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import interPlugin from "@fullcalendar/inter";
+import interactionPlugin from "@fullcalendar/interaction";
+import './styles/fullCalendarStyles.css'
+import styles from "./styles/Calendar.module.css"
 
 
 export default function Calendar({ events = [{title: 'Hello World', start: '2026-06-03'}] }) {
   const [selectedDate, setSelectedDate] = useState(null)
+  const handleClick = (cell) => {
+    setSelectedDate(cell.dateStr)
+    console.log(cell.dateStr)
+
+  }
     return (
     <FullCalendar
       plugins={[dayGridPlugin, interactionPlugin]}
       initialView="dayGridMonth"
       events={events}
+      dateClick={handleClick}
+      dayCellClassNames={(cell) => {
+        let formattedDate = cell.date.toISOString().split('T')[0]
+        if (selectedDate && formattedDate === selectedDate) {      
+            return [styles.activeDate]
+        }
+        return []
+      }}
     />
   );
 }
