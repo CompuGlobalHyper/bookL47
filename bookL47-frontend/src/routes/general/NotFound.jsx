777d6c7cfa909dbf9e5db883a80ce568b50f5fa5
CompuGlobalHyper <phineascrisp@gmail.com>
2026-07-20 14:18:10 -0700

    Removed loading testing

--- bookL47-frontend/src/routes/general/NotFound.jsx
+++ bookL47-frontend/src/routes/general/NotFound.jsx
@@ -5,9 +5,6 @@ import Loading from '../../components/Loading'
 export default function NotFound() {
 
   return (
-    <Loading></Loading>
-  )
-  return (
     <div>
       <h1 className='text'>Error 404</h1>
       <p className='text'>We couldn't find the page you were looking for..</p>
